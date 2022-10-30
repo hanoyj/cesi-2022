8cff1f6653e624ea185642e88265d688aa685b9a
hanoyj <julien.hanoy@gmail.com>
2022-10-30 21:42:41 +0100

    Ajout schéma UML

--- PBL2/PROSIT2/CLExec.h
+++ PBL2/PROSIT2/CLExec.h
@@ -5,12 +5,14 @@ namespace CryptoClients
 	// Classe permettant l'interaction avec un utilisateur
 	class CLExec
 	{
-	public:
-		// Méthode principale de traitement
-		static void run();
+	private:
 		// Affichage d'un entete
 		static void enTete();
 		// Affichage d'un menu
 		static void menu();
+
+	public:
+		// Méthode principale de traitement
+		static void run();
 	};
 }
